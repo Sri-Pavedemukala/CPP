33dbb62a51ba4f30d4796c8a89f37e76c4db785d
EC2 Default User <ec2-user@ip-172-31-44-240.eu-north-1.compute.internal>
2023-04-26 20:19:50 +0000

    first

---
+++ models.py
@@ -0,0 +1,263 @@
+"""SQLAlchemy models for makeup_world app."""
+
+from datetime import datetime
+from wtforms import SubmitField, BooleanField, StringField,IntegerField,  TextAreaField,SelectField, PasswordField, validators
+from flask_wtf import Form
+
+from  flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
+
+from flask_bcrypt import Bcrypt
+
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+
+
+GENERIC_IMAGE = "https://cdn.pixabay.com/photo/2016/02/19/11/35/makeup-1209798_1280.jpg"
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+class Follows(db.Model):
+    """Connection of a follower <-> followed_user."""
+
+    __tablename__ = 'follows'
+
+    user_being_followed_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete="cascade"),
+        primary_key=True,
+    )
+
+    user_following_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete="cascade"),
+        primary_key=True,
+    )
+
+
+class Likes(db.Model):
+    """Mapping user likes to warbles."""
+
+    __tablename__ = 'likes' 
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='cascade')
+    )
+
+    message_id = db.Column(
+        db.Integer,
+        db.ForeignKey('messages.id', ondelete='cascade'),
+        unique=True
+    )
+
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    header_image_url = db.Column(
+        db.Text,
+        default="/static/images/warbler-hero.jpg"
+    )
+
+    bio = db.Column(
+        db.Text,
+    )
+
+    location = db.Column(
+        db.Text,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    messages = db.relationship('Message')
+
+    followers = db.relationship(
+        "User",
+        secondary="follows",
+        primaryjoin=(Follows.user_being_followed_id == id),
+        secondaryjoin=(Follows.user_following_id == id)
+    )
+
+    following = db.relationship(
+        "User",
+        secondary="follows",
+        primaryjoin=(Follows.user_following_id == id),
+        secondaryjoin=(Follows.user_being_followed_id == id)
+    )
+
+    likes = db.relationship(
+        'Message',
+        secondary="likes",
+        backref="users"
+    )
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    def is_followed_by(self, other_user):
+        """Is this user followed by `other_user`?"""
+
+        found_user_list = [user for user in self.followers if user == other_user]
+        return len(found_user_list) == 1
+
+    def is_following(self, other_user):
+        """Is this user following `other_use`?"""
+
+        found_user_list = [user for user in self.following if user == other_user]
+        return len(found_user_list) == 1
+
+    @classmethod
+    def signup(cls, username, email, password, image_url):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+
+class Message(db.Model):
+    """An individual message ("warble")."""
+
+    __tablename__ = 'messages'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    text = db.Column(
+        db.String(140),
+        nullable=False,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow(),
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    user = db.relationship('User')
+
+
+
+class  Product(db.Model):
+    """Select product."""
+
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer,primary_key=True)
+    brand = db.Column(db.Text,nullable=False)
+    name = db.Column(db.Text,nullable=False)
+    photo_url =db.Column(db.Text)
+    price = db.Column(db.Integer)
+    review = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable = False,default= True)
+
+    def image_url(self):
+        """Return image for product -- bespoke or generic."""
+
+        return self.photo_url or GENERIC_IMAGE
+
+
+
+####################
+
+class RegForm(Form):
+    name_first = StringField('First Name', [validators.DataRequired()])
+    name_last = StringField('Last Name', [validators.DataRequired()])
+    email = StringField('Email Address', [validators.DataRequired(), validators.Email(), validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    submit = SubmitField('Submit')
+
+##################
+
+
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
